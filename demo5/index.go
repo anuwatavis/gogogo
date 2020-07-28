d63a4ebd5b6a37f20a6b01933600e4115b6b7691
anuwataravis <anuwat.avis@gmail.com>
2020-07-28 11:20:13 +0700

    Define Function + Return

--- demo5/index.go
+++ demo5/index.go
@@ -9,6 +9,13 @@ func main() {
 	display()
 	fn2("Good Morning")
 	fn3("number is", 2)
+	//return function
+	const a, b = 2, 3
+	//d stand for decimal
+	fmt.Printf("%d+%d=%d\n", a, b, sum(a, b))
+	var x, y int = swap(a, b)
+	fmt.Printf("x = %d and y = %d | a = %d b = %d", x, y, a, b)
+
 }
 
 func display() {
@@ -23,4 +30,12 @@ func fn2(msg string) {
 func fn3(title string, version int) {
 	fmt.Println(title)
 	fmt.Println(version)
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func swap(a int, b int) (int, int) {
+	return b, a
 }
