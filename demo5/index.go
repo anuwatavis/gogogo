ced87c1e37ca1afabab05f6252300bd3850247f2
anuwataravis <anuwat.avis@gmail.com>
2020-07-28 11:28:24 +0700

    Define Function + Named Return

--- demo5/index.go
+++ demo5/index.go
@@ -14,7 +14,9 @@ func main() {
 	//d stand for decimal
 	fmt.Printf("%d+%d=%d\n", a, b, sum(a, b))
 	var x, y int = swap(a, b)
-	fmt.Printf("x = %d and y = %d | a = %d b = %d", x, y, a, b)
+	fmt.Printf("x = %d and y = %d | a = %d b = %d \n", x, y, a, b)
+	_x, _y, title := swap2(10, 20)
+	fmt.Printf("x = %d and y = %d, string = %s | a = %d b = %d", _x, _y, title, 10, 20)
 
 }
 
@@ -38,4 +40,11 @@ func sum(a int, b int) int {
 
 func swap(a int, b int) (int, int) {
 	return b, a
+}
+
+func swap2(a int, b int) (x, y int, title string) {
+	y = a
+	x = b
+	title = "Result"
+	return
 }
