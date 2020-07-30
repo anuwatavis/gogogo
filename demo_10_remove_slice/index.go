b3575c7dbdb9576c4ec9693bdda4f931380e6ebb
anuwataravis <anuwat.avis@gmail.com>
2020-07-30 12:16:07 +0700

    remove specific index in array

--- demo_10_remove_slice/index.go
+++ demo_10_remove_slice/index.go
@@ -15,8 +15,16 @@ func main() {
 	//tailing remove
 	numbers = numbers[0 : len(numbers)-1]
 	showSlice(numbers)
+
+	//remove specific index
+	numbers = removeIndex(numbers, 2)
+	showSlice(numbers)
 }
 
 func showSlice(x []int) {
 	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
+}
+
+func removeIndex(s []int, index int) []int {
+	return append(s[:index], s[index+1:]...) //... spreading all array
 }
