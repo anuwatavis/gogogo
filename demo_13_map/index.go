2f6304e693b0d6de5e49e9f0d67608177bcd4c26
anuwataravis <anuwat.avis@gmail.com>
2020-07-30 13:04:03 +0700

    Make Map to Dynamic

--- demo_13_map/index.go
+++ demo_13_map/index.go
@@ -5,4 +5,13 @@ import "fmt"
 func main() {
 	var numbers = map[string]int{"one": 1, "two": 2, "three": 3}
 	fmt.Println("", numbers["two"])
+
+	//dynamic map
+
+	var colors = make(map[string]string)
+	colors["red"] = "#F00"
+	colors["green"] = "#0F0"
+	colors["blue"] = "#00F"
+	fmt.Println("", colors)
+	fmt.Println("", colors["green"])
 }
